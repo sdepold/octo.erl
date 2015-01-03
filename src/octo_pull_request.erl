69141b49c4f2482e3c3c775b6b89e08925bd3e3b
Sascha Depold <sascha@depold.com>
2015-01-03 20:12:40 +0100

    Move array transformation out of api

--- src/octo_pull_request.erl
+++ src/octo_pull_request.erl
@@ -11,14 +11,17 @@ list(Owner, Repo) ->
   Url          = "https://api.github.com/repos/" ++ Owner ++ "/" ++ Repo ++ "/pulls",
   Json         = octo_helper:get(Url),
   PullRequests = jsonerl:decode(Json),
+  pull_request_blobs_to_record_list(PullRequests).
+
+read(Owner, Repo, Number) ->
+  Url  = "https://api.github.com/repos/" ++ Owner ++ "/" ++ Repo ++ "/pulls/" ++ integer_to_list(Number),
+  Json = octo_helper:get(Url),
+  ?json_to_record(octo_pull_request, Json).
+
+pull_request_blobs_to_record_list(PullRequests) ->
   lists:map(
     fun(PullRequest) ->
       ?struct_to_record(octo_pull_request, PullRequest)
     end,
     PullRequests
   ).
-
-read(Owner, Repo, Number) ->
-  Url  = "https://api.github.com/repos/" ++ Owner ++ "/" ++ Repo ++ "/pulls/" ++ integer_to_list(Number),
-  Json = octo_helper:get(Url),
-  ?json_to_record(octo_pull_request, Json).
