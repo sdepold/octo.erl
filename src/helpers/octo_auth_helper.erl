6a6c29d3491c20c3d4e4663b1a08763d03a2c5d2
Alexander Batischev <eual.jp@gmail.com>
2016-03-14 19:29:06 +0300

    Make auth helper emit headers only for known options

--- src/helpers/octo_auth_helper.erl
+++ src/helpers/octo_auth_helper.erl
@@ -7,9 +7,9 @@ parse_options([]) ->
   [];
 
 %% Authentication via Personal API token: https://github.com/blog/1509-personal-api-tokens
-parse_options([{ auth, pat, Pat } | Rest]) ->
+parse_options([{ auth, pat, Pat } | _Rest]) ->
   Header = {"Authorization", "Basic " ++ base64:encode_to_string(Pat ++ ":x-oauth-basic")},
-  [Header | Rest];
+  [Header];
 
 parse_options([_|Rest]) ->
   parse_options(Rest).
