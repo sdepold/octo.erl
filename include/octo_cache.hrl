0d68e586d68de48b304164738765cbfd9ba873d0
Alexander Batischev <eual.jp@gmail.com>
2016-03-15 22:19:42 +0300

    Cache Link header values

--- include/octo_cache.hrl
+++ include/octo_cache.hrl
@@ -1,6 +1,7 @@
 -record(octo_cache_headers, {
           etag          = undefined,
-          last_modified = undefined}).
+          last_modified = undefined,
+          link          = []}).
 -record(octo_cache_entry, {
           headers = #octo_cache_headers{},
           result  = undefined}).
--- src/octo_cache.erl
+++ src/octo_cache.erl
@@ -72,6 +72,7 @@ handle_call({request, Method, Url, OctoOpts, Payload, Opts}, _From, State) ->
       NewState = update_ratelimit(RespHeaders, State),
 
       store_caching_headers(CacheKey, RespHeaders),
+      store_link_header(CacheKey, RespHeaders),
 
       case StatusCode of
         304 ->
@@ -188,6 +189,32 @@ store_caching_headers(CacheKey, Headers) ->
                   Updated = EntryHeaders#octo_cache_headers{
                               etag          = ETag,
                               last_modified = Last_Modified},
+                  Entry#octo_cache_entry{headers = Updated}
+              end);
+       true -> ok
+  end.
+
+store_link_header(undefined, _Headers) -> ok;
+store_link_header(CacheKey, Headers) ->
+  Link = hackney_headers:parse(<<"Link">>, Headers),
+
+  if Link =/= undefined ->
+       %% Turn the value into proplist with keys "next", "prev", "first and
+       %% "last"
+       Value = lists:map(
+                 fun(String) ->
+                     [URL, Rel] = string:tokens(String, ";"),
+                     {match, [Key|_]} = re:run(Rel,
+                                               "^ rel=\"\(.*\)\"$",
+                                               [{capture, [1], list}]),
+                     {list_to_atom(Key), URL}
+                 end,
+                 string:tokens(binary:bin_to_list(Link), ",")),
+       ok = internal_update_cache(
+              CacheKey,
+              fun(Entry) ->
+                  EntryHeaders = Entry#octo_cache_entry.headers,
+                  Updated = EntryHeaders#octo_cache_headers{link = Value},
                   Entry#octo_cache_entry{headers = Updated}
               end);
        true -> ok
